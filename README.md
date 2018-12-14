46abaa866abd1ae31a2abffb078a0703b4d728eb
Mr. Goferito <goferito@gmail.com>
2018-12-14 16:30:51 +0100

    Prompt auth + README

--- README.md
+++ README.md
@@ -3,15 +3,66 @@
 [![NPM license][npm-license-img]][npm-url]
 
 # Termllo
-Trello on the Terminal
+*Trello on the Terminal*
+
+So far it implements basic Trello features (select board, add card with title
+and description, move cards around and edit cards), so it's just useful for
+solo boards (not that cool for colaborative boards (yet)).
+
+**Note:** Only starred boards appear for now. So starring at least one board via
+Trello Web or the Trello App is necessary to see something in Termllo.
+
+More advance features will be implemented on the future. If someone out there
+ever gets to really use this, any feedback would be really appreciated.
+
+```
+//TODO Screenshot here
+```
+
+*Made with <3 by Mr. Goferito*
+
 
 ## Usage
-1. `cp auth.js.example auth.js` and fill `auth.js` in with your data.
-2. `npm i`
-3. `node app.js`
+Navigation is Vim-inspired:
+
+```
+* h, j, k, l  -> Move around
+* H, J, K, L  -> Move selected card around
+* Enter, o, e -> Open card dialog (for the selected card)
+* n           -> New Card (on the active list)
+* b           -> Boards dialog (just starred boards)
+* Esc, q      -> Close dialog (boards or card)
+* Ctrl+c      -> Terminate
+```
+
+## Installation
+First you need to get your key & token from trello:
+[https://trello.com/app-key](https://trello.com/app-key)
+
+Then there are two options:
+
+### NPM Install (recommended)
+1. `npm i -g termllo`
+2. `termllo`
+3. Introduce your key and token when prompted
+* This will generate a file in `$HOME/.termllo/auth.js`. You can also
+create that file yourself from the `auth.js.example` on the root of this repo*
+
+### Clone this repo
+1. `git clone https://github.com/goferito/termllo.git`
+2. `cd termllo && npm i`
+3. Copy/move the `auth.js.example` to `auth.js`, and edit it with your data
+4. `node app.js`
 
 ## TODO
-* Check for trello server data from time to time to corroborate termllo state
+- [ ] Check for trello server data from time to time to corroborate termllo state
+- [x] Prompt for trello token if missing, and save it in $HOME/.termllo/auth.js
+- [ ] Add screenshot to readme
+- [ ] Delay and pool move requests to minimize api calls
+- [ ] Notify new version available
+- [ ] Checklist support on the cards
+- [ ] Lables support
+- [ ] Type ? for help of shortcuts
 
 ## On development
 Nodemon and blessed don't play very well together. To avoid nodemon
@@ -28,3 +79,4 @@ MIT © Sadoht Gomez Fernandez
 [npm-downloads-img]: https://img.shields.io/npm/dm/termllo.svg
 [npm-version-img]: https://badge.fury.io/js/termllo.svg
 [npm-license-img]: https://img.shields.io/npm/l/termllo.svg
+
--- app.js
+++ app.js
@@ -1,494 +1,76 @@
 
-// Check if auth file is there
-try {
-  require('./auth')
-} catch (e) {
-  console.error('\nERROR: No file \'auth.js\' found.\n'
-  + 'You need to \'cp auth.js.example auth.js\' and fill it with your data.')
-  process.exit(1)
-}
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const inquirer = require('inquirer')
+const replace = require('replace-in-file')
 
-
-const blessed = require('blessed')
 const TrelloManager = require('./TrelloManager')
-
-const auth = require('./auth')
-
-const MIN_COL_WIDTH = 30
+const ui = require('./lib/ui')
 
 
-/****************************  MVP  *************************
- *                                                          *
- *  jsonfile cache properly working
- *                                                          *
- *  select board dialog
- *                                                          *
- ************************************************************/
+// Release the Kraken! (aka, run this thing)
+getAuth()
+  .then((auth) => {
+
+  const tMan = new TrelloManager(auth)
+
+  tMan.fillStore()
+  .then((store) => ui.drawUI(store, tMan))
+})
 
 
-const tMan = new TrelloManager(auth)
-tMan.fillStore()
-.then(drawUI)
 
-function drawUI (store) {
+/**
+ * Checks for existing auth files. If none, prompts for key and token
+ * and stores the on $HOME/.termllo/auth.js
+ */
+async function getAuth () {
 
-  const screen = blessed.screen({
-    smartCSR: true,
-    title: 'Termllo',
-    dockBorders: true,
+  // Check if local auth
+  if (fs.existsSync("./auth.js")) {
+    return require('./auth')
+  }
+
+  // Create config file on $HOME
+  const homedir = os.homedir()
+  const termlloDir = path.join(homedir, '.termllo')
+  if (!fs.existsSync(termlloDir)) {
+    fs.mkdirSync(termlloDir)
+  }
+
+  // Check for home auth
+  const homeAuth = path.join(homedir, '.termllo', 'auth.js')
+  if (fs.existsSync(homeAuth)) {
+    return require(homeAuth)
+  }
+
+  // Prompt for auth
+  console.log('\nAuth config missing. Please:')
+  console.log(' 1. Get your api key and token from https://trello.com/app-key')
+  console.log(' 2. Copy paste them here:')
+  const answers = await inquirer.prompt([
+    { type: 'input', name: 'key',   message: 'Your API Key:' },
+    { type: 'input', name: 'token', message: 'Your Token:'    },
+  ])
+
+  // Copy example to $HOME/.termllo/auth.js
+  const pathExample = path.join(__dirname, './auth.js.example')
+  fs.copyFileSync(pathExample, homeAuth)
+
+
+  // Replace key and token with placeholders
+  await replace({
+    files: homeAuth,
+    from: 'your-key',
+    to: answers.key,
+  })
+  await replace({
+    files: homeAuth,
+    from: 'your-token',
+    to: answers.token,
   })
 
-  // Quit on Control-C.
-  screen.key('C-c', () => process.exit(0))
-
-  // Show boards dialog
-  screen.key('b', () => {
-    boardList.show()
-    boardList.focus()
-  })
-
-  const listBoxes = tMan.lists.map((list, i) => {
-    const lBox = blessed.list({
-      left: i * 30,
-      width: 30,
-      height: '95%',
-      keys: false,
-      items: list.cards.map(card => card.name),
-      border: {
-        type: 'line',
-      },
-      label: `[ ${list.name} ]`,
-      scrollbar: true,
-      style: {
-        label: {
-          fg: 'gray',
-          bold: true,
-        },
-        selected: {
-          bg: 'black',
-          fg: 'white',
-        },
-        item: {
-          bg: 'black',
-          height: '100',
-        },
-        focus: {
-          selected: {
-            bg: 'blue',
-            bold: true,
-          },
-          border: {
-            fg: 'cyan',
-          },
-          label: {
-            fg: 'cyan',
-          },
-        },
-        scrollbar: {
-          bg: 'blue',
-        },
-      },
-    })
-
-    // Select next card
-    lBox.key(['j'], () => lBox.down(1))
-
-    // Select prev card
-    lBox.key(['k'], () => lBox.up(1))
-
-    // Select list on the left
-    lBox.key(['h'], () => {
-      const numLists = tMan.lists.length
-      tMan.activeBoardIdx = Math.max(0, tMan.activeBoardIdx - 1)
-      listBoxes[tMan.activeBoardIdx].focus()
-
-      const screenWidth = process.stdout.columns
-      const nCols = Math.floor(screenWidth / MIN_COL_WIDTH)
-      const offset = Math.max(tMan.activeBoardIdx - (nCols - 1), 0) * MIN_COL_WIDTH
-      listBoxes.forEach((box, i) => {
-        box.left = MIN_COL_WIDTH * i - offset
-      })
-
-      screen.render()
-    })
-
-    // Select list on the right
-    lBox.key(['l'], () => {
-      const numLists = tMan.lists.length
-      tMan.activeBoardIdx = Math.min(numLists - 1, tMan.activeBoardIdx + 1)
-      listBoxes[tMan.activeBoardIdx].focus()
-
-      const screenWidth = process.stdout.columns
-      const nCols = Math.floor(screenWidth / MIN_COL_WIDTH)
-      const offset = Math.max(tMan.activeBoardIdx - (nCols - 1), 0) * MIN_COL_WIDTH
-      listBoxes.forEach((box, i) => {
-        box.left = MIN_COL_WIDTH * i - offset
-      })
-
-      screen.render()
-    })
-
-    // Insert new card
-    lBox.key(['n'], () => {
-      cardForm.reset()
-      cardForm.setLabel('[ New Card ]')
-      cardForm.show()
-      titleBox.focus()
-      screen.render()
-    })
-
-    // Select active card
-    lBox.key(['enter', 'o', 'e'], () => {
-      const card = tMan.cards[list.id][lBox.selected]
-      tMan.editingCard = card
-      titleBox.setValue(card.name)
-      descrBox.setValue(card.desc)
-      cardForm.setLabel('[ Edit Card ]')
-      cardForm.show()
-      cardForm.focus()
-      screen.render()
-    })
-
-    // Move card to left list
-    lBox.key(['S-h'], () => {
-      if (i < 1) return
-      const toListIdx = i - 1
-      const toList = tMan.lists[toListIdx].id
-      const toPos = lBox.selected
-      const fromList = list.id
-      const fromPos = lBox.selected
-
-      tMan.moveCard({fromList, fromPos, toList, toPos})
-      .then((newItems) => {
-        lBox.setItems(newItems.fromList)
-
-        // Update desination box
-        const destBox = listBoxes[toListIdx]
-        destBox.setItems(newItems.toList)
-        destBox.select(toPos)
-        destBox.focus()
-      })
-    })
-
-    // Move card to right list
-    lBox.key(['S-l'], () => {
-      if (i > tMan.lists.length - 2) return
-      const toListIdx = i + 1
-      const toList = tMan.lists[toListIdx].id
-      const toPos = lBox.selected
-      const fromList = list.id
-      const fromPos = lBox.selected
-
-      tMan.moveCard({fromList, fromPos, toList, toPos})
-      .then((newItems) => {
-        lBox.setItems(newItems.fromList)
-
-        // Update desination box
-        const destBox = listBoxes[toListIdx]
-        destBox.setItems(newItems.toList)
-        destBox.select(toPos)
-        destBox.focus()
-      })
-    })
-
-    // Move card up
-    lBox.key(['S-k'], () => {
-      if (lBox.selected === 0) return
-      const fromList = list.id
-      const fromPos = lBox.selected
-      const toList = list.id
-      const toPos = fromPos - 1
-
-      tMan.moveCard({fromList, fromPos, toList, toPos})
-      .then((newItems) => {
-        lBox.setItems(newItems.fromList)
-      })
-    })
-
-    // Move card down
-    lBox.key(['S-j'], () => {
-      const numCards = lBox.items.length
-      const fromList = list.id
-      const fromPos = lBox.selected
-      if (fromPos > numCards - 2) return
-      const toList = list.id
-      const toPos = fromPos + 1
-      tMan.moveCard({fromList, fromPos, toList, toPos})
-      .then((newItems) => {
-        lBox.setItems(newItems.fromList)
-      })
-    })
-
-
-    return lBox
-  })
-
-  listBoxes.forEach((list) => {
-    screen.append(list)
-  })
-  listBoxes[0].focus()
-
-  const boardList = blessed.list({
-    parent: screen,
-    shadow: true,
-    left: '25%',
-    width: '50%',
-    top: '35%',
-    height: '25%',
-    keys: false,
-    items: tMan.boards.map(b => b.name),
-    border: {
-      type: 'line',
-    },
-    label: `[ Boards ]`,
-    scrollbar: true,
-    style: {
-      label: {
-        fg: 'gray',
-        bold: true,
-      },
-      selected: {
-        bg: 'black',
-        fg: 'white',
-      },
-      item: {
-        bg: 'black',
-        height: '100',
-      },
-      focus: {
-        selected: {
-          bg: 'blue',
-          bold: true,
-        },
-        border: {
-          fg: 'cyan',
-        },
-        label: {
-          fg: 'cyan',
-        },
-      },
-      scrollbar: {
-        bg: 'blue',
-      },
-    },
-  })
-
-  // Select next board
-  boardList.key(['j'], () => boardList.down(1))
-
-  // Select prev board
-  boardList.key(['k'], () => boardList.up(1))
-
-  // Close board dialog
-  boardList.key(['q', 'escape'], () => boardList.hide())
-  boardList.key(['enter'], () => {
-    tMan.fillStore({boardIdx: boardList.selected})
-    .then((store) => {
-      screen.destroy()
-      drawUI(store)
-    })
-    .catch((err) => {
-      console.error(err)
-      process.exit(1)
-    })
-  })
-
-
-  const cardForm = blessed.form({
-    parent: screen,
-    top: '10%',
-    left: '10%',
-    width: '80%',
-    height: '80%',
-    keys: true,
-    vi: true,
-    shadow: true,
-    padding: {
-      top: 1,
-      left: 1,
-      right: 1,
-      bottom: 0,
-    },
-    border: {
-      type: 'line',
-    },
-    style: {
-      label: {
-        fg: 'green',
-        left: 'center',
-        bold: true,
-      },
-      border: {
-        fg: 'green',
-      },
-    },
-  })
-  const titleBox = blessed.textbox({
-    parent: cardForm,
-    name: 'name',
-    height: 3,
-    keys: true,
-    vi: true,
-    inputOnFocus: true,
-    border: {
-      type: 'line',
-    },
-    label: ` Title `,
-    style: {
-      label: {
-        fg: 'green',
-      },
-      border: {
-        fg: 'green',
-      },
-      focus: {
-        label: {
-          bold: true,
-        },
-        border: {
-          bold: true,
-        }
-      }
-    },
-  })
-  const descrBox = blessed.textarea({
-    parent: cardForm,
-    name: 'desc',
-    top: 3,
-    keys: true,
-    vi: true,
-    inputOnFocus: true,
-    border: {
-      type: 'line',
-    },
-    label: ` Description `,
-    style: {
-      label: {
-        fg: 'green',
-        left: '50%',
-      },
-      border: {
-        fg: 'green',
-      },
-      focus: {
-        label: {
-          bold: true,
-        },
-        border: {
-          bold: true,
-        },
-      }
-    },
-  })
-  const submitBtn = blessed.button({
-    parent: descrBox,
-    mouse: true,
-    shadow: true,
-    keys: true,
-    shrink: true,
-    padding: {
-      top: 1,
-      left: 3,
-      right: 3,
-      bottom: 1,
-    },
-    top: '100%-5',
-    left: '50%',
-    shrink: true,
-    name: 'submit',
-    content: 'Submit',
-    style: {
-      bg: 'blue',
-      focus: {
-        bg: 'red'
-      },
-      hover: {
-        bg: 'red'
-      }
-    }
-  })
-  const cancelBtn = blessed.button({
-    parent: descrBox,
-    keys: true,
-    shrink: true,
-    mouse: true,
-    padding: {
-      top: 1,
-      left: 3,
-      right: 3,
-      bottom: 1,
-    },
-    left: '70%',
-    top: '100%-5',
-    shrink: true,
-    name: 'cancel',
-    content: 'Cancel',
-    style: {
-      bg: 'blue',
-      focus: {
-        bg: 'red'
-      },
-      hover: {
-        bg: 'red'
-      }
-    }
-  })
-  cancelBtn.on('press', () => {
-    cardForm.reset()
-    cardForm.hide()
-    listBoxes[tMan.activeListIdx].focus()
-    screen.render()
-  })
-  submitBtn.on('press', () => cardForm.submit())
-  cardForm.key('enter', () => cardForm.submit())
-  cardForm.on('submit', (card) => {
-    if (!card.name) return
-
-    const list = tMan.lists[tMan.activeBoardIdx]
-    let actionFn
-    if (tMan.editingCard) {
-
-      // If no change, just close
-      if (tMan.editCard.name === card.name && tMan.editCard.desc === card.desc) {
-        cardForm.reset()
-        cardForm.hide()
-      }
-
-      actionFn = 'editCard'
-      card = Object.assign(tMan.editingCard, card)
-    } else {
-      actionFn = 'createCard'
-      card.idList = list.id
-    }
-
-    tMan[actionFn](card)
-    .then(() => {
-      cardForm.reset()
-      cardForm.hide()
-      listBoxes[tMan.activeListIdx].focus()
-      const newItems = tMan.cards[list.id].map(card => card.name)
-      listBoxes[tMan.activeListIdx].setItems(newItems)
-      if (!tMan.editingCard) {
-        listBoxes[tMan.activeListIdx].select(0)
-     }
-      tMan.editingCard = false
-      screen.render()
-    })
-    .catch((e) => { throw e })
-  })
-
-  // Cancel and close
-  cardForm.key(['escape', 'c', 'q', 'u'], () => {
-    cardForm.reset()
-    cardForm.hide()
-  })
-
-  // Hidden at start
-  cardForm.hide()
-  boardList.hide()
-
-
-  // Initial render
-  screen.render()
+  return answers
 }
 
